50106f2816c0da0072b612f1324895dafc255087
Lemonfaace <jasteads@gmail.com>
2026-05-27 00:40:29 -0500

    Editor improvements
    Added labelling buttons and replaced the textarea element with a div + spans. Still need to properly learn CSS.

--- JS/blog-editor.js
+++ JS/blog-editor.js
@@ -1,3 +1,45 @@
+const editorBody = document.getElementById('editor-body');
+const editorContent = document.getElementById('editor-content');
+const editorPlaceholder = document.getElementById('editor-placeholder');
+
+/**
+ * Gives the target button the ability to augment text
+ * @param {string} type 
+ * @param {HTMLElement} button 
+ */
+const assignLabel = (type, button) => {
+    if (!button) return;
+
+    const activateLabelButton = () => {
+        const selection = window.getSelection();
+
+        // Make sure only the text editor is being modified
+        if (!editorBody.contains(selection.anchorNode)) return;
+
+        console.log('Selection area found');
+        
+        // Properties setup
+        const range = selection.getRangeAt(0);
+        const targetText = range.toString();
+        const labeledText = `[${type}]${targetText}[/${type}]`;
+        const textNode = document.createTextNode(labeledText);
+        
+        // Clear the selected area, replace with the labeled text, and prepare reselection
+        range.deleteContents();
+        range.insertNode(textNode);
+        range.setStart(textNode, 0);
+        range.setEnd(textNode, textNode.length);
+        
+        // Finally, reselect using the new selection range
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        updatePlaceholder(); // Update placeholder text in case it's empty
+    }
+
+    button.addEventListener('click', () => activateLabelButton(type));
+}
+
 /**
  * Creates a blog post record for the database.
  * @param {string} title 
@@ -31,8 +73,59 @@ const publishBlog = async () => {
     else alert('Title must be defined.');
 }
 
+const updatePlaceholder = () => {
+    const isEmpty = editorContent.innerText.trim() === '';
+    console.log(isEmpty);
+
+    editorPlaceholder.style.display = isEmpty ? 'inline' : 'none';
+}
+
+/**
+ * 
+ * @param {ClipboardEvent} e 
+ */
+const fixPaste = e => {
+    e.preventDefault(); // Don't paste yet
+
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+
+    if (!editorContent.contains(range.commonAncestorContainer)) return;
+
+    const plainText = e.clipboardData.getData('text/plain');
+    const textNode = document.createTextNode(plainText);
+
+    range.deleteContents();
+    range.insertNode(textNode);
+    range.setStart(textNode, textNode.length);
+    range.collapse(true);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    updatePlaceholder();
+}
+
 const initializeEditor = () => {
     const publishButton = document.getElementById('publish-button');
+    
+    editorBody.addEventListener('click', () => editorContent.focus());
+
+    editorBody.addEventListener('input', updatePlaceholder);
+    editorBody.addEventListener('keyup', updatePlaceholder);
+    editorBody.addEventListener('focus', updatePlaceholder);
+    editorBody.addEventListener('blur', updatePlaceholder);
+    editorBody.addEventListener('paste', e => fixPaste(e));
+
+    updatePlaceholder();
+
+    // Initialize formatting tools
+    assignLabel('b', document.getElementById('bold-button'));
+    assignLabel('i', document.getElementById('em-button'));
+    assignLabel('u', document.getElementById('u-button'));
+    assignLabel('url', document.getElementById('url-button'));
+    assignLabel('img', document.getElementById('image-button'));
+    assignLabel('size', document.getElementById('size-button'));
 
     publishButton.addEventListener('click', publishBlog);
 }
